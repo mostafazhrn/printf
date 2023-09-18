2ca178daffbcb9f330002bfbcf82f6287c389e29
mostafazhrn <mostafazhrn@gmail.com>
2023-09-18 16:57:03 -0400

    newupdates

--- function2.c
+++ function2.c
@@ -1,8 +1,6 @@
 #include "main.h"
-
-/****************** PRINT POINTER ******************/
 /**
- * print_pointer - Prints the value of a pointer variable
+ * print_pointer - this code shallPrints the value of a pointer variable
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
