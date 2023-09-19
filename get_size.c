01b0578de86bb1623e8c156a6d41c1e75c39bdc3
mostafazhrn <mostafazhrn@gmail.com>
2023-09-19 06:02:10 -0400

    updatesminor

--- get_size.c
+++ get_size.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * get_size - Calculates the size to cast the argument
- * @format: Formatted string in which to print the arguments
- * @i: List of arguments to be printed.
- *
- * Return: Precision.
+ * @format: this shall rep Formatted string in which to print the arguments
+ * @i: this hsall rep List of arguments to be printed.
+ * Return: shall return Precision.
  */
 int get_size(const char *format, int *i)
 {
